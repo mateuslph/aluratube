1e16cec4d3f0d9092e35aae9dbc8debc2ae0c859
Mateus L. Pereira <mlptche@gmail.com>
2023-07-29 14:20:26 -0300

    dark mode e button cadastrar

--- src/components/Menu/components/Search.js
+++ src/components/Menu/components/Search.js
@@ -34,17 +34,23 @@ const StyledSearch = styled.div`
   }
 `;
 
+// Home 
+// Menu
+// Search
+// Informação sempre desce
+
 export default function Search({ valorDoFiltro, setValorDoFiltro }) {
-    // const [valorDaBusca, setValorDaBusca] = React.useState("Modern");
-    // console.log("Search", valorDaBusca);
-    const ValorDaBusca = valorDoFiltro;
+    // const [valorDaBusca, setValorDaBusca] = React.useState("Teste");
+    // console.log("Search", valorDaBusca)
+    const valorDaBusca = valorDoFiltro;
     const setValorDaBusca = setValorDoFiltro;
+
     return (
         <StyledSearch>
-            <input type="text" onChange={(infosDoEvento) => setValorDaBusca(infosDoEvento.target.value)} value={ValorDaBusca} />
+            <input type="text" onChange={(e) => setValorDaBusca(e.target.value)} value={valorDaBusca} />
             <button>
                 🔎
-            </button>            
+            </button>
         </StyledSearch>
     )
 }
